e495911c6c06661a0439c5c51fd6e1cbeb2abe8e
ShiZhongCheng <2454715873@qq.com>
2018-01-22 12:45:40 +0800

    修改入口文件index.php内容并且移动其位置，修改了配置文件的路径

--- index.php
+++ index.php
@@ -12,6 +12,13 @@
 // [ 应用入口文件 ]
 
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
+define('APP_PATH', __DIR__ . '/application/');
+
+//定义配置文件目录
+define('CONF_PATH',__DIR__ . '/conf/');
+
+//静态配置文件路径
+define('PEIZHI_PATH',__DIR__ . '/public/');
+
 // 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+require __DIR__ . '/thinkphp/start.php';
